006823dde9f9f486f7f220a51167b8e76fb27eb0
FACILITE\aparis <aparis@facilite.com>
2017-02-21 14:16:13 -0500

    session 29

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './components/global/Menu.jsx';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search'
+
+const API_KEY = 'AIzaSyBjIsZ0pHo0QpBZvh6BIGmrc_bE0TLNnJI'
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            videos : [],
+            selectedVideo : null
+        }
+
+        YTSearch({key : API_KEY, term : 'surfboards'}, (videos) => {
+            this.setState({
+                videos : videos,
+                selectedVideo : videos[0]
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    <Menu />,
+    <App />,
     document.getElementById('app')
 );
---
+++ app/js/components/video_list.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import VideoListItem from './video_list_item'
+
+// class VideoList extends React.Component {
+//     render() {
+//         return (
+//             <ul className="col-md-4 list-group">
+//
+//             </ul>
+//         )
+//     }
+// }
+const VideoList = (props) => {
+    return (
+        <ul className="col-md-4 list-group">
+            { props.videos.map(video => <VideoListItem key={video.etag} video={video}/>) }
+        </ul>
+    )
+}
+
+export default VideoList
