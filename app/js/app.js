83a4de29c21765954d696e6856caabaf40d73ef3
FACILITE\aparis <aparis@facilite.com>
2017-02-22 10:22:04 -0500

    do not reset selectedVideo when search term changed

--- app/js/app.js
+++ app/js/app.js
@@ -23,8 +23,7 @@ class App extends Component {
     videoSearch(term) {
         YTSearch({key : API_KEY, term : term}, (videos) => {
             this.setState({
-                videos : videos,
-                selectedVideo : videos[0]
+                videos : videos
             })
         })
     }
