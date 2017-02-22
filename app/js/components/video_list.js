4cbdd1f9f6028ad144adb8be73330e80f1a2736f
FACILITE\aparis <aparis@facilite.com>
2017-02-22 09:07:29 -0500

    remove dead code

--- app/js/components/video_list.js
+++ app/js/components/video_list.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import VideoListItem from './video_list_item'
 
-// class VideoList extends React.Component {
-//     render() {
-//         return (
-//             <ul className="col-md-4 list-group">
-//
-//             </ul>
-//         )
-//     }
-// }
 const VideoList = (props) => {
     return (
         <ul className="col-md-4 list-group">
